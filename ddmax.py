bbe58a800aeef7022666fe24eab6b0ed99bcb8e3
Rahul Gopinath <rahul@gopinath.org>
2022-03-03 05:08:54 -0800

    .

--- ddmax.py
+++ ddmax.py
@@ -61,8 +61,8 @@ def ddmax2(cprime_y, n):
     #Fig 5: else, if n < len(delta), increase granularity
     #note: CX_minus_cprime_y = CX_S - cprime_y
     if n < len(CX_minus_cprime_y):
-        # Fig 5: ddmax2(c'y, min(|cx|, 2n))
-        #return ddmax2(cprime_y, min(len(CX_S), 2*n)) # XXX: LIKELY BUGGY
+        # Fig 5: ddmax2(c'y, min(|cx|, 2n))  <-- this is buggy
+        #return ddmax2(cprime_y, min(len(CX_S), 2*n)) # XXX: LIKELY BUGGY but from Fig 5.
         return ddmax2(cprime_y, min(len(CX_minus_cprime_y), 2*n)) # THIS WILL WORK: 
     #else:
     return cprime_y
