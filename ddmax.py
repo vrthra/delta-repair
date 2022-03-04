8c5388da114f4778c5be7e4af761fd1c20d85056
Rahul Gopinath <rahul@gopinath.org>
2022-03-04 01:49:25 -0800

    .

--- ddmax.py
+++ ddmax.py
@@ -49,8 +49,7 @@ def increase_grannularity(n, CX_minus_cprime_y):
 def ddmax2(cprime_y, n):
     # Base case where the number of excluded bytes from the input has a size of
     # 1, i.e. cannot be minimized further
-    if (len(cprime_y) + 1) == len(CX_S):
-        print("DeltaSet is 1-minimal.")
+    if len(minus(CX_I, cprime_y)) == 1: # NOT in Fig 5.
         return cprime_y
 
     CX_minus_cprime_y = minus(CX_I, cprime_y)
@@ -81,7 +80,7 @@ def ddmax2(cprime_y, n):
     #Fig 5: else, if n < len(delta), increase granularity
     if increase_grannularity(n, CX_minus_cprime_y):
         # Fig 5: ddmax2(c'y, min(|cx|, 2n))  <-- this is buggy
-        return ddmax2(cprime_y, min(len(CX_S) - len(cprime_y), 2*n)) # XXX: BUGGY but from Fig 5.
+        return ddmax2(cprime_y, min(len(minus(CX_I,cprime_y)), 2*n)) # XXX: BUGGY but from Fig 5.
 
     # Fig5: otherwise done
     return cprime_y
